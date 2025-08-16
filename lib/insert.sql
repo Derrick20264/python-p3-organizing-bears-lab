9c8a86bd0d047af1c29b25489db2fefd0f07987b
derrick <derrick.mwangi2@student.moringaschool.com>
2025-08-16 10:34:52 +0300

    Fix bear queries and update select_all_bears_names_and_orders_in_alphabetical_order

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1, 'Mr. Chocolate', 20, 'M', 'Brown', 'Calm', 1),
+(2, 'Rowdy', 10, 'M', 'Black', 'Aggressive', 1),
+(3, 'Tabitha', 6, 'F', 'Dark Brown', 'Friendly', 1),
+(4, 'Sergeant Brown', 19, 'M', 'Dark Brown', 'Serious', 0),
+(5, 'Melissa', 13, 'F', 'Black', 'Calm', 1),
+(6, 'Grinch', 2, 'M', 'Green', 'Grumpy', 1),
+(7, 'Wendy', 7, 'F', 'White', 'Playful', 1),
+(8, NULL, 3, 'M', 'Grey', 'Confused', 1);
